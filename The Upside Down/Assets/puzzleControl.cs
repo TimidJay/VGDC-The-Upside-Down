1adebfbb02dac46bbb426d046d4ac4de13a21c1a
TimidJay <justizl1@uci.edu>
2017-01-25 14:02:30 -0800

    Created prefabs for an Inventory UI
    The junk that's pushed with this keeps bothering me.

---
+++ The Upside Down/Assets/puzzleControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzleControl : MonoBehaviour
+{
+
+    private int room, currentPuzzle, puzzlePhase;
+    //
+    private bool isDisturbed;
+
+    //I'm hoping that we can just use this one script on the girl to control all "states" of a room based on where in the puzzle the girl is
+
+    // Use this for initialization
+    void Start()
+    {
+        room = 1;
+        currentPuzzle = 1;
+        puzzlePhase = 1;
+        isDisturbed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
