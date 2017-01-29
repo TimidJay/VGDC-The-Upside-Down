4dc790c64ab226be019b74db65c91c3b85eb7c91
bradcruz <bradd@uci.edu>
2017-01-28 18:20:03 -0800

    Script Files - Player and Menu Control

---
+++ The Upside Down/Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+    public Vector2 targetPosition;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            print(targetPosition);
+        }
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 5);
+	}
+}
