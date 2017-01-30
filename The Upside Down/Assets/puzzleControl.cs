d3728e61e0af728e96150bc4fc03eee5b6bf2066
TimidJay <justizl1@uci.edu>
2017-01-30 13:24:05 -0800

    A few adjustments to my test room and puzzleControl
    Still working out how to get buttons to work, still despairing over
    github

--- The Upside Down/Assets/puzzleControl.cs
+++ The Upside Down/Assets/puzzleControl.cs
@@ -6,8 +6,9 @@ public class puzzleControl : MonoBehaviour
 {
 
     private int room, currentPuzzle, puzzlePhase;
-    private string selectedInvItem,thingClicked;
-    //
+    private string selectedInvItem, thingClicked;
+
+    private string[] inventory;
     private bool isDisturbed;
 
     //I'm hoping that we can just use this one script on the girl to control all "states" of a room based on where in the puzzle the girl is
@@ -19,8 +20,15 @@ public class puzzleControl : MonoBehaviour
         currentPuzzle = 1;
         puzzlePhase = 1;
         isDisturbed = false;
+
+        //Set to inventory item when you click on that inv slot
         selectedInvItem = "empty";
-        thingClicked = "nothing";
+        
+        inventory = new string[5];
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            inventory[i] = "empty";
+        }
     }
 
     // Update is called once per frame
@@ -41,17 +49,69 @@ public class puzzleControl : MonoBehaviour
             //same as puzzlePhase==12 and any flavor texts that function at that time
         }
 
+        print("In disturbed:" + isDisturbed.ToString());
+
     }
-    void setThingClicked(string clicked)
+    //Gets and sets
+    void setThingClicked(string clicked){thingClicked = clicked;}
+    void setInvItem(string item){selectedInvItem = item;}
+
+    void nextPuzzle(){currentPuzzle += 1;}
+    void nextPhase(){puzzlePhase += 1;}
+
+    public void flipWorlds(){isDisturbed = !isDisturbed;}
+
+    public int getPuzzle(){return currentPuzzle;}
+    public int getPhase(){return puzzlePhase;}
+    public string getInvItem(){return selectedInvItem;}
+    public string[] getInv() { return inventory; }
+
+
+    public void addToInv(string s)
     {
-        thingClicked = clicked;
+        //Adds an item to the inventory if there isn't already an instance of that item in the inventory
+        //Assuming we set up a fetch quest where items can be picked up in any order, we'd have to make it so we can't submit the items until
+        //we reach the right puzzlePhase (where each item pickup increases puzzlePhase by 1, hopefully)
+        int empty = -1;
+        bool alreadyThere = false;
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if (inventory[i] == "empty")
+            {
+                empty = i;
+                break;
+            }
+            if (inventory[i] == s)
+            {
+                alreadyThere = true;
+                break;
+            }
+        }
+        if (!alreadyThere && empty > -1)
+        {
+            inventory[empty] = s;
+        }
     }
-    void setInvItem(string item)
+
+    public void removeFromInv(string s)
     {
-        selectedInvItem = item;
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if (inventory[i] == s)
+            {
+                inventory[i] = "empty";
+                break;
+            }
+        }
+        print("Item not found");
     }
-    void flipWorlds()
+
+    public bool conditionCheck(string neededItem)
     {
-        isDisturbed = !isDisturbed;
+        if (selectedInvItem == neededItem)
+            return true;
+        return false;
     }
+    
+
 }
